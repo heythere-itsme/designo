485657d822316d8d1ec344b55fa8c1c43f5a6cfb
Mayank_Kumar <mayankkumar.current@gmail.com>
2025-09-27 13:20:05 +0530

    have to

---
+++ src/app/auth/sign-in/page.tsx
@@ -0,0 +1,10 @@
+import { LoginForm } from '@/components/login-form'
+import React from 'react'
+
+const page = () => {
+  return (
+    <LoginForm className='w-100 mx-auto mt-40' />
+  )
+}
+
+export default page
